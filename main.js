4eb0fc8844a830b77e4c3a26e6eb83bfa42bbdc0
SaparovaSevara <saparovasevaraxon@gmail.com>
2022-07-11 21:32:08 +0500

    Update file

---
+++ main.js
@@ -0,0 +1,11 @@
+// Swipper
+
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    speed: 400,
+    spaceBetween: 0,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
